a441d5122734fbb884a1afc20f9fd4581aea8ff5
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-04-28 00:54:43 +0300

    Update app.js

--- 04-Operators/app.js
+++ 04-Operators/app.js
@@ -4,7 +4,7 @@
 
 
 
-// //Bölme / ve mod % operatörü
+// //---Bölme / ve mod % operatörü
 // let sayı1=500;
 // let sayi2= 23;
 // let sonuc;
@@ -28,7 +28,7 @@
 
 
 
-// //Artırma, eksiltme operatörleri.
+// //---Artırma, eksiltme operatörleri.
 // let counter = 0;
 // console.log("Counter: "+counter); 
 // counter = counter + 1;
@@ -42,9 +42,30 @@
 // console.log("Counter: "+counter);
 
 
-let counter = 10;
-console.log(counter++);//Önce değer kullanılır, sonra işlem yapılır. //counter i konsola yaz, sonra değeri 1 artır. Bu satır bittiğinde counter değeri 11 ancak ekranda 10  görünür.Ne zaman tekrar yazdırırsak o zaman 11 görülür.
+// let counter = 10;
+// console.log(counter++);//Önce değer kullanılır, sonra işlem yapılır. //counter i konsola yaz, sonra değeri 1 artır. Bu satır bittiğinde counter değeri 11 ancak ekranda 10  görünür.Ne zaman tekrar yazdırırsak o zaman 11 görülür.
 
-console.log(counter);
+// console.log(counter);
 
-console.log(++counter);//Önce işlem yapılır, sonra bu değer kullanılır. //Bir üst satırda cpunter değeri 11, bu satırsa 11 e 1 ekler 12 yapar ve ekrana yazdırır.
+// console.log(++counter);//Önce işlem yapılır, sonra bu değer kullanılır. //Bir üst satırda cpunter değeri 11, bu satırsa 11 e 1 ekler 12 yapar ve ekrana yazdırır.
+
+// console.log(counter++ + counter-- + ++counter + --counter);
+
+
+
+// //---Karşılaştırma Operatörleri
+// let sayi1 =20;
+// let sayi2 = 30;
+
+// console.log(sayi1>sayi2); //sayı1 sayı2 den büyükmü sarusu sorulur, false sonucu alınır.
+
+// let sayi3 =50;
+// let sayi4 = 30;
+
+// console.log(sayi3 > sayi4); //sayı3 sayı4 den büyükmü sarusu sorulur, true sonucu alınır.
+// console.log(sayi3 >= sayi4); //Büyük Eşit
+// console.log(sayi3 <= sayi4); //Küçük Eşit
+// console.log(sayi3 == sayi4); //Eşittir.
+// console.log(sayi3 != sayi4); //Eşit değildir, farklıdır..
+
+
