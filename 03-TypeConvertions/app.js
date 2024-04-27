b6abfde7854cd340b292328abc059714087a1b65
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-04-28 00:36:55 +0300

    +-

--- 03-TypeConvertions/app.js
+++ 03-TypeConvertions/app.js
@@ -9,8 +9,24 @@
 // console.log(priceString, typeof priceString);
 
 
-let firstName = "Müslüm";
-let midleName = "Han";
-let lastName = "Erol"
-let fullName = firstName+" "+midleName+" "+lastName;
-console.log(fullName);
+// let firstName = "Müslüm";
+// let midleName = "Han";
+// let lastName = "Erol"
+// let fullName = firstName+" "+midleName+" "+lastName;
+// console.log(fullName);
+
+// ---Number
+// console.log("6" / "3");
+// console.log(Number(65+1));
+
+// let value1=true;
+// let value2=false;
+// console.log(Number(value1)); //true nin numara karşılığını gösterir.
+// console.log(Number(value2));
+
+
+// ---Boolean
+
+console.log(Boolean(1)); //1 in karşılığının gösterir true.
+console.log(Boolean(0));
+
---
+++ 04-Operators/app.js
@@ -0,0 +1,50 @@
+// // = atama operatörü
+// let x;
+// x = 45;
+
+
+
+// //Bölme / ve mod % operatörü
+// let sayı1=500;
+// let sayi2= 23;
+// let sonuc;
+
+//  sonuc = sayı1 / sayi2; //bölme
+
+// sonuc = sayı1 % sayi2; //mod alma. kalanı bulma
+
+// console.log(sonuc);
+
+
+
+// // karesini alma
+// let x;
+// x = 45;
+// console.log(2 ** 2);
+// console.log(2 ** 4);
+// console.log(25 ** 2);
+
+// console.log(27 ** (1/3)); //küp kökü alma.
+
+
+
+// //Artırma, eksiltme operatörleri.
+// let counter = 0;
+// console.log("Counter: "+counter); 
+// counter = counter + 1;
+// console.log("Counter: "+counter);
+// counter++;
+// console.log("Counter: "+counter);
+
+// counter--;
+// console.log("Counter: "+counter);
+// counter--;
+// console.log("Counter: "+counter);
+
+
+let counter = 10;
+console.log(counter++);//Önce değer kullanılır, sonra işlem yapılır. //counter i konsola yaz, sonra değeri 1 artır. Bu satır bittiğinde counter değeri 11 ancak ekranda 10  görünür.Ne zaman tekrar yazdırırsak o zaman 11 görülür.
+
+console.log(counter);
+
+console.log(++counter);//Önce işlem yapılır, sonra bu değer kullanılır. //Bir üst satırda cpunter değeri 11, bu satırsa 11 e 1 ekler 12 yapar ve ekrana yazdırır.
