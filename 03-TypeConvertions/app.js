303124d0b7b1c737c8d3daa8562e927a3ab1f395
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-04-27 23:58:34 +0300

    Update app.js

--- 03-TypeConvertions/app.js
+++ 03-TypeConvertions/app.js
@@ -1,8 +1,16 @@
 // let ısActive = true;
 // alert(typeof ısActive);
 
-let price =10000;
-console.log(price, typeof price); 
 
-let priceString = String(price); //tip dönüştürüldü.
-console.log(priceString, typeof priceString);
+// let price =10000;
+// console.log(price, typeof price); 
+
+// let priceString = String(price); //tip dönüştürüldü.
+// console.log(priceString, typeof priceString);
+
+
+let firstName = "Müslüm";
+let midleName = "Han";
+let lastName = "Erol"
+let fullName = firstName+" "+midleName+" "+lastName;
+console.log(fullName);
