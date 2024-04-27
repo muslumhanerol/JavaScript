c60475c239ab485b4f0fb5530c0b9a807f9926e2
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-04-27 23:54:52 +0300

    type_con

---
+++ 03-TypeConvertions/app.js
@@ -0,0 +1,8 @@
+// let ısActive = true;
+// alert(typeof ısActive);
+
+let price =10000;
+console.log(price, typeof price); 
+
+let priceString = String(price); //tip dönüştürüldü.
+console.log(priceString, typeof priceString);
