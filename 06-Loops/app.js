8ed3395ef2366d099920e4267401640ace160995
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-04-29 00:39:49 +0300

    loop

---
+++ 06-Loops/app.js
@@ -0,0 +1,54 @@
+//wHİLE döngüsü
+
+// Koşul doğru olduğu sürece kod çalışır.
+//1 10 arası sayıları yazdırdık.
+// let a=1;
+// while (a <= 10) { 
+//     console.log(a);
+//     a++;
+// }
+//-------------------------------------------
+
+//1 le 10 arası çift sayıları yazdır.
+// let i=2;
+// while (i<=10) {
+//     console.log(i);
+//     i=i+2;
+
+// }
+
+
+//1 le 10 arası çift sayıları yazdır.mod ile
+// let i=1;
+// while (i<=10) {
+//     if (i % 2 == 0){ //i nin 2 ye bölümünden kalan 0 mı sorusu sorduk.
+//         console.log(i);
+//     }    
+//     i++;
+// }
+//---------------------------------------------------------
+
+//1 ile 50 arasındaki 4 ün katı olan sayıların adetini bul.
+// let i =1;
+// let sayac = 0;
+
+// while (i < 50) {
+//     if(i % 4 == 0){
+//         sayac++;
+//     }
+//     i++;
+// }
+// console.log(sayac);
+//------------------------------------------------------------------
+
+//FOR
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+
+
+
+
