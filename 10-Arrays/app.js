969946a7647113ef71b07374d5f7cceb0c740f50
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-05-01 00:10:10 +0300

    arrays

---
+++ 10-Arrays/app.js
@@ -0,0 +1,9 @@
+//Diziler: birden fazla değeri içinde tutan veri yapıları.
+
+let products=[];
+products[0]="Samsung s10+++";
+products[1]="Samsung s20+++";
+products[10]="Samsung s30+++";
+
+
+console.log(products);
