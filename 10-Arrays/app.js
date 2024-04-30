df95e1fd4fd8e4729b734d1314c12a07a0a3d471
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-05-01 00:40:00 +0300

    arraysMethdot

--- 10-Arrays/app.js
+++ 10-Arrays/app.js
@@ -1,9 +1,22 @@
 //Diziler: birden fazla değeri içinde tutan veri yapıları.
 
-let products=[];
-products[0]="Samsung s10+++";
-products[1]="Samsung s20+++";
-products[10]="Samsung s30+++";
+// let products=[];
+// products[0]="Samsung s10+++";
+// products[1]="Samsung s20+++";
+// products[10]="Samsung s30+++";
 
+// console.log(products);
+// console.log(products[0]); //Dizinin ilk elemanını verir.
+// console.log(products[1]); //Dizinin ikinci elemanını verir.
+
+//-------------------------------------------------
+
+let products = ["Samsung","Iohone","Vestel","Arçelik"]
 
 console.log(products);
+
+let prices = [20000,25000,19999,5400];
+console.log(prices);
+
+let student1 = ["Müslüm","Erol",30,false]
+console.log(student1);
---
+++ 10-ArraysMethods/app.js
@@ -0,0 +1,12 @@
+let students = ["Ayşe","Umay","Ceyda","Köfte","Erol"];
+
+let result;
+
+result = students.length;
+result = students;
+result = students.toString(); //Dizinin tüm elemanlarını araya virgül koyarak yazdırır.
+result = students.join("-"); //Dizinin aralarına ekler. Tek bir değer olarak çıktı verir.
+result = result.split(""); //Harf harf ayırır.
+
+
+console.log(result);
