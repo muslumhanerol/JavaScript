81de58af1fcd7d73c5e74d611836f7443800f44d
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-05-01 23:08:43 +0300

    Update app.js

--- 11-Object/app.js
+++ 11-Object/app.js
@@ -1,1 +1,10 @@
-//Java Script Object Notation -- JSON
+//Java Script Object Notation - JSON 
+
+let student1 = ["Müslüm","Erol",30,"Erkek"];
+let student2 = ["Onur","Hasan",28,"Erkek"];
+let student3 = ["Ayşe","Mayşe",25,"Kadın"];
+
+let students = [student1,student2,student3];
+console.log(students);
+
+
