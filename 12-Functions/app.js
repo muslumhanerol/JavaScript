ff48cbc2f66e248462b67eff6c8f87270604e7ff
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-05-02 00:40:40 +0300

    func

---
+++ 12-Functions/app.js
@@ -0,0 +1,23 @@
+// function selamVer(){         //parametresiz function
+//     console.log("Merhaba");
+// }
+
+// selamVer();
+// selamVer();
+// selamVer();
+
+//------------------------------------
+
+// function selamVer2(message) {   //parametreli function
+//     console.log(message);
+// }
+
+// selamVer2("Merhaba, nasılsın?"); //1.selamver2 komutu burada görününce 11.satır  function çalışacak. Bunu yaparken string ifadeyi "Merhaba, nasılsın?" (message) değişkeninin içine yerleştirecek. Yani message = "Merhaba, nasılsın?" dır. Bu yüzden console.log(message); kodunun çıktısı "Merhaba, nasılsın?" olur.
+
+//---------------------------------------
+
+function today (){
+    console.log(new Date());
+}
+
+today();
