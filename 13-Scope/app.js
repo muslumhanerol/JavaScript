70c1cda345e520d1543c0161fa2580a441e550d7
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-05-04 00:35:51 +0300

    DOM

--- 13-Scope/app.js
+++ 13-Scope/app.js
@@ -1,12 +1,33 @@
-let name = "Müslüm Erol";
+
+// function print(){
+//     let name = "Müslüm Erol"; //function scope içinde tanımlanan değişken sadece o function içinde geçerlidir.
+//     console.log(name);
+// }
+
+// if(true){
+//     let price = 45;
+//     console.log(name);
+// }
+
+// print();
+
+// console.log(price);
+
+//ES5 de değişken tanımlamak için var ve const kullanılırdı.
+//ES5 le beraber bunlara let eklendi.
+
+//-------------------------------------------------------------
+
+var name = "Müslüm Erol"
 
 function print(){
     console.log(name);
 }
 
 if(true){
-    console.log(name);
-
+    var price = 45; //var ile değişken tanımlarsak if, for gibi yerlerde değişken kullanılabilir, let te olmaz yukarıdaki örnekte old. gibi. 
 }
-
 print();
+
+console.log(name);
+console.log(price);
